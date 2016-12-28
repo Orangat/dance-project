c98b1c7b25f2bc9e0a84a1d96b6a0986436eb843
Sergey Nazarov <dr.orangat@gmail.com>
2016-12-28 22:53:48 +0200

    adaptive procese

--- js/main.js
+++ js/main.js
@@ -6,16 +6,84 @@
   // 	 slidesToScroll: 3
   // }); 
 
+
+// 
+
 $(document).ready(function(){
   $('.sl').slick({
    	
    	infinite: true,
    	dots: true,
    	centerMode: true,
- 	slidesToShow: 5,
- 	centerPadding: '60px',
- 	
- 	autoplay: true,
+ 		slidesToShow: 5,
+ 		centerPadding: '60px',
+ 		autoplay: true,
+    autoplaySpeed: 3000,
+
+     responsive: [
+    {
+      breakpoint: 1044,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 780,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  
+  ]
+ 
+  	});
+  });
+
+
+$(document).ready(function(){
+   $('.sl_two').slick({
+   	infinite: true,
+   	dots: true,
+ 		autoplay: true,
     autoplaySpeed: 3000,
   });
-});
+ });
+
+
+
+
+
+  $(".text-trigger").click(function () {
+      $(".some-div").slideToggle(400);
+    });
+
+  $(".text-trigger2").click(function () {
+      $(".some-div2").slideToggle(400);
+    });
+
+  $(".text-trigger3").click(function () {
+      $(".some-div3").slideToggle(400);
+    });
+
+  $(".text-trigger4").click(function () {
+      $(".some-div4").slideToggle(400);
+    });
+
+  $(".text-trigger5").click(function () {
+      $(".some-div5").slideToggle(400);
+    });
+
+
+
+// 
